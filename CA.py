1c026e91c09df07a0e7e80e859bc6fe6d446e303
JohnNinan117 <48561958+JohnNinan117@users.noreply.github.com>
2022-11-15 18:43:07 -0500

    Create CA.py

---
+++ CA.py
@@ -0,0 +1,17 @@
+'''
+    *  Full Name:
+    *  Course:EECS 3482 A
+    *  Description: Certification Authority 
+    *
+'''
+
+
+#Generate a public/private key pair for the certificate authority. The public key of the CA is self-signed.
+def generateCAkeys():
+    return 
+
+def generateCertificate(user_id, user_pub_key):
+
+    return 
+
+    
